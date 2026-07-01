7c3a90001148a083c5804fb53f25e7008ffca203
Lokesh Fulara <your_email@gmail.com>
2026-07-01 16:45:29 +0530

    commit done

---
+++ PRACTIES/FIRST/second.js
@@ -0,0 +1,22 @@
+let obj={
+    name:"rohit",
+    name:28,
+    balence:444,
+    gender:"male",
+    meet:function(){
+        console.log("Hello Coder Army");
+    }
+}
+console.log(obj);
+
+function updatebalence(balence){
+    balence+=50;
+    return balence;
+}
+console.log(updatebalence(222));
+
+function updatename(name){
+    name=name;
+    return name;
+}
+console.log(updatename("Rohit Sharma"));
